8d5f3b06e5c8145ce74e093bfd4ad60ec30c2107
Phil1436 <philxsb@gmail.com>
2023-08-26 09:29:07 +0200

    update

---
+++ src/utils/Dir.ts
@@ -0,0 +1,3 @@
+import { Entry } from './Entry';
+
+export class Dir extends Entry {}
---
+++ src/utils/Entry.ts
@@ -0,0 +1,6 @@
+export class Entry {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
